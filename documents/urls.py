70eead31cbd648859882a1c67bade27d98b3ff62
mrity <mrityunjay.100293@gmail.com>
2025-06-15 22:34:59 -0700

    views updated with new codes

--- documents/urls.py
+++ documents/urls.py
@@ -10,16 +10,5 @@ urlpatterns = [
     path('download/<int:application_id>/', views.DownloadDocumentsView.as_view(), name='download'),
     path('download-all/<int:search_id>/', views.DownloadAllDocumentsView.as_view(), name='download_all'),
     path('preview/<int:application_id>/<str:doc_type>/', views.PreviewDocumentView.as_view(), name='preview'),
-    # # Document management
-    # path('preview/<int:application_id>/', views.DocumentPreviewView.as_view(), name='preview'),
-    # path('download/<int:application_id>/', views.DocumentDownloadView.as_view(), name='download'),
-    # path('download/<int:application_id>/<str:doc_type>/', views.SingleDocumentDownloadView.as_view(),
-    #      name='download_single'),
-    #
-    # # Document generation
-    # path('generate/<int:application_id>/', views.GenerateDocumentsView.as_view(), name='generate'),
-    # path('regenerate/<int:application_id>/', views.RegenerateDocumentsView.as_view(), name='regenerate'),
-    #
-    # # Document status
-    # path('status/<int:application_id>/', views.DocumentStatusView.as_view(), name='status'),
+
 ]
