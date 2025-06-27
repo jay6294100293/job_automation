34b93e94cf9d22a5d49af23a8c5b8e6166b56a05
mrity <mrityunjay.100293@gmail.com>
2025-06-27 12:49:47 -0700

    final productions server db

--- job_automation/settings.py
+++ job_automation/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# env_path = BASE_DIR / '.debug.env'
 env_path = BASE_DIR / '.env'
 load_dotenv(env_path)
 
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'job_automation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME', 'resume_db'),
-#         'USER': os.getenv('DB_USER', 'resume_user'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', 'resume_password'),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-#
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'resume_db'),
+        'USER': os.getenv('DB_USER', 'resume_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'resume_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
